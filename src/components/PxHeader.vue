3d4abe02e00f4793ea913a96fc970420f04ae0f1
edwingallardo516 <edwingallardo516@gmail.com>
2021-10-23 15:59:14 -0500

    Cascaron de diseño

---
+++ src/components/PxHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="shadow w-screen">
+        <nav>
+            <nav class="flex items-center justify-between flex-wrap p-6 bg-orange-500 text-gray-100">
+                <div class="flex items-center flex-shrink-0 mr-6">
+                    <PxIcon class="mr-3"/>
+                    <span class="font-semibold text-xl tracking-tight colo"> Exchange </span>
+                </div>
+                <div class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto">
+                   <div class="text-sm lg:flex-grow"></div>
+                </div>
+            </nav>
+        </nav>
+    </header>
+</template>
+
+<script>
+import PxIcon from './PxIcon.vue'
+export default {
+    name: 'PxHeader',
+    components: { PxIcon }
+}
+</script>
